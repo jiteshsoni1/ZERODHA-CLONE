0f57d57f045627a023b7323ead1e8dcab3fe089e
jiteshsoni1 <jiteshsoni3591@gmail.com>
2026-03-19 16:37:16 +0530

    working crud operations

---
+++ backend/controllers/userController.js
@@ -0,0 +1,100 @@
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const { MongoClient } = require("mongodb");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const url = process.env.MONGO_URL;
+
+let client;
+
+async function connectClient() {
+  if (!client) {
+    client = new MongoClient(url, {
+      // useNewUrlParser: true,
+      // useUnifiedTopology: true,
+    });
+    await client.connect();
+  }
+}
+
+async function signup(req, res) {
+  const { username, password, email } = req.body;
+  try {
+    await connectClient();
+    const db = client.db("zerodhaclonecluster");
+    const usersCollection = db.collection("users");
+
+    const user = await usersCollection.findOne({ username });
+    if (user) {
+      return res.status(400).json({ message: "User already exists!" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = {
+      username,
+      password: hashedPassword,
+      email,
+       };
+
+    const result = await usersCollection.insertOne(newUser);
+
+    const token = jwt.sign(
+      { id: result.insertedId },
+      process.env.JWT_SECRET_KEY,
+      { expiresIn: "1h" }
+    );
+    res.json({ token, userId: result.insertedId });
+  } catch (err) {
+    console.error("Error during signup : ", err.message);
+    res.status(500).send("Server error");
+  }
+}
+
+async function login(req, res) {
+  const { email, password } = req.body;
+  try {
+    await connectClient();
+    const db = client.db("zerodhaclonecluster");
+    const usersCollection = db.collection("users");
+
+    const user = await usersCollection.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials!" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials!" });
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET_KEY, {
+      expiresIn: "1h",
+    });
+    res.json({ token, userId: user._id });
+  } catch (err) {
+    console.error("Error during login : ", err.message);
+    res.status(500).send("Server error!");
+  }
+}
+
+async function getAllUsers(req, res) {
+  try {
+    await connectClient();
+    const db = client.db("zerodhaclonecluster");
+    const usersCollection = db.collection("users");
+
+    const users = await usersCollection.find({}).toArray();
+    res.json(users);
+  } catch (err) {
+    console.error("Error during fetching : ", err.message);
+    res.status(500).send("Server error!");
+  }
+}
+module.exports = {
+  getAllUsers,
+  signup,
+  login,
+};
